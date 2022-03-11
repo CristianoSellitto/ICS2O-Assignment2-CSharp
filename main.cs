b00c0fc30e69bec331115a39532053126b6c0c71
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-11 18:27:12 +0000

    Added template

--- main.cs
+++ main.cs
@@ -1,7 +1,12 @@
+// Cristiano | ICS2O-Assignment2-CSharp | March 11 2022
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine("\nFinished.");
+    }
 }
