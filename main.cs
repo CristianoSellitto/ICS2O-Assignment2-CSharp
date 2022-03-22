64b9e99d394ef17f49ad675b0e3efd5185237514
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-03-22 17:29:39 +0000

    Changed target shape

--- main.cs
+++ main.cs
@@ -8,8 +8,15 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("The area of a rectangle with a length of 5 and a width of 3 is:");
-      Console.WriteLine((5 * 3) + "cm squared");
+        int length;
+        int width;
+        Console.WriteLine("The area of a triangle with a length of 5 and a width of 3 is:");
+        Console.WriteLine((5 * 3) + "cm²");
+        Console.WriteLine("Add the length of your triangle:");
+        length = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Add the width of your triangle:");
+        width = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Your triangle's area is " + (length * width / 2) + "cm²");
         Console.WriteLine("\nFinished.");
     }
 }
